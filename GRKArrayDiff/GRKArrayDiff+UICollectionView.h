9c2e055512e9ef4bdbccf8dce4316768c81aafe0
Levi Brown <levigroker@gmail.com>
2018-02-14 14:22:59 -0700

    Updating license

--- GRKArrayDiff/GRKArrayDiff+UICollectionView.h
+++ GRKArrayDiff/GRKArrayDiff+UICollectionView.h
@@ -2,17 +2,12 @@
 //  GRKArrayDiff+UICollectionView.h
 //
 //  Created by Michael Kessler on April 28, 2016.
-//  Copyright (c) 2016 Levi Brown <mailto:levigroker@gmail.com>
-//  This work is licensed under the Creative Commons Attribution 3.0
-//  Unported License. To view a copy of this license, visit
-//  http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative
-//  Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
-//  USA.
-//
-//  The above attribution and the included license must accompany any version
-//  of the source code. Visible attribution in any binary distributable
-//  including this work (or derivatives) is not required, but would be
-//  appreciated.
+//  Copyright (c) 2016-2018 Levi Brown <mailto:levigroker@gmail.com> This work is
+//  licensed under the Creative Commons Attribution 4.0 International License. To
+//  view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
+//  
+//  The above attribution and this license must accompany any version of the source
+//  code, binary distributable, or derivatives.
 //
 
 #import <Foundation/Foundation.h>
@@ -30,11 +25,11 @@
  *
  * @param collectionView The target collection view to update.
  * @param section        The target section of the table view.
- * @param animation      The animation style to perform when updating the table.
  * @param completion     A completion block which will be called once the table has been updated (and animations, if any, have completed). This can be `nil`.
  */
 - (void)updateCollectionView:(UICollectionView *)collectionView section:(NSInteger)section completion:(void(^)(void))completion;
 
 #endif
+ * @param animation      The animation style to perform when updating the table.
 
 @end
